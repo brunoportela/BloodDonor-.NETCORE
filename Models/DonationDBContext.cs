2a9ffac6db99b7e0e714a920bb430d527d48f092
brunoportela <13480425+brunoportela@users.noreply.github.com>
2020-11-17 11:52:08 -0800

    Initial Commit - .NET Core

---
+++ Models/DonationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class DonationDBContext:DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
